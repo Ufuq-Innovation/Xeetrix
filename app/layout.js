beaf93470c02a1b4f6c0712351b15e98e2bdeeb4
Shaikh Baset <148635255+shaikhbaset2001@users.noreply.github.com>
2025-12-29 23:29:17 +0600

    layout update

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-background text-white">{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
