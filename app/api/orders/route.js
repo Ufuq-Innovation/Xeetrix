b4b965fe429989066cf1226dbc5f738382b133a6
Shaikh Baset <abdulbaset3227@gmail.com>
2026-01-02 23:07:20 +0600

    Final path fix for build

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,5 +1,5 @@
-import dbConnect from "../../lib/mongodb";
-import order from "../../models/order";
+import dbConnect from "../../../lib/mongodb"; 
+import order from "../../../models/order";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
