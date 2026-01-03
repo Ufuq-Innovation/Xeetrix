c4bfee4b40c56a74e02c9c8874a1ed7ace0ac7aa
Shaikh Baset <abdulbaset3227@gmail.com>
2026-01-03 13:41:48 +0600

    Fixed database name case sensitivity

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -4,7 +4,7 @@ import clientPromise from "@/lib/mongodb";
 export async function POST(req) {
   try {
     const client = await clientPromise;
-    const db = client.db("Xeetrix"); 
+    const db = client.db("xeetrix"); 
     const body = await req.json();
 
     console.log("Received Order Data:", body); // ভার্সেল লগে ডাটা চেক করার জন্য
